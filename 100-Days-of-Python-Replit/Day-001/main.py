21fd372d88af57d80e6295fd3e3f6472c922a72f
RyanADouglas <136330853+RyanADouglas@users.noreply.github.com>
2023-08-07 09:19:32 -0400

    Day_1 main.py

--- 100-Days-of-Python-Replit/Day-001/main.py
+++ 100-Days-of-Python-Replit/Day-001/main.py
@@ -1,1 +1,13 @@
-
+print("Ryan")
+print("August 7th 2023 ")
+print("""
+I am signing up for Replit's 100 days of python
+challenge!
+I will make sure to spend some time every day coding
+along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do
+this from my phone wherever I happen to be. No excuses
+for not coding from the middle of a field!
+I am feeling 🥳
+you can follow my progress at https://github.com/RyanADouglas/100-Days-of-Python-Replit
+""")
