0eebe5e0765d4df51f7e7e95f9cdc5c59cbf6635
RyanADouglas <136330853+RyanADouglas@users.noreply.github.com>
2023-08-09 16:09:06 -0400

    Create main.py

---
+++ 100-Days-of-Python-Replit/Day-003/main.py
@@ -0,0 +1,8 @@
+food = input("Name a Food: ")
+plant = input("Name a type of plant: ")
+cooking = input("How do you like your food cooked? ")
+burnedFood = input("Describe burned food in one word: ")
+item = input("Name a home tool: ")
+
+print("MENU")
+print("For lunch you should make", cooking, food, "with", burnedFood, plant, "with a side of", item) 
